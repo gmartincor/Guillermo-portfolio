750aad11823bd885fe36b64f25e64d0e935e3021
gmartincor <guillermomc007@gmail.com>
2025-08-07 14:01:42 +0200

    Remove 'Built with React and lots of ☕' from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-white dark:bg-gray-900 py-12 text-center border-t border-gray-200 dark:border-gray-800">
       <p className="text-gray-600 dark:text-gray-400 text-lg">
-        © 2025 Guillermo Martín Correa. Built with React and lots of ☕
+        © 2025 Guillermo Martín Correa
       </p>
     </footer>
   );
